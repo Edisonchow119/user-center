bfc87a0a77085037e83060c4e97bdfde12e4b295
edison <edison-chow@foxmail.com>
2024-03-22 17:20:21 +0800

    📝 docs(compiler): update support prod-environment; add Dockerfile support dockers-build

--- src/app.tsx
+++ src/app.tsx
@@ -16,6 +16,8 @@ const loginPath = '/user/login';
 const NO_NEED_LOGIN_WHITE_LIST = ['/user/register', loginPath];
 
 export const request: RequestConfig = {
+  // 生成环境则下访问
+  baseURL: process.env.NODE_ENV === 'production' ? 'http://user-backend.code-nav.cn' : undefined,
   timeout: 1000000,
 };
 
